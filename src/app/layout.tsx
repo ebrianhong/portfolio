f8bd979d02d85283e5100c7d6f035e5aa716399e
Brian Hong <ebrianhong@gmail.com>
2024-01-19 16:17:04 -0800

    move navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "./components/NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Brian\'s Portfolio Website',
-  description: 'Brian\'s Portfolio Website',
-}
+  title: "Brian's Portfolio Website",
+  description: "Brian's Portfolio Website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar></NavBar>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Welcome from "./components/Welcome";
-import NavBar from "./components/NavBar";
 
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <NavBar></NavBar>
       <Welcome></Welcome>
       <Experience></Experience>
       <Skills></Skills>
